d2fb3c85e6ba827531d4b01b91e189e6ea350e37
SIMORRE Eric <eric.simorre@csgroup.eu>
2022-02-23 18:49:16 +0100

    add a splashscreen

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react/destructuring-assignment */
 import { render } from 'react-dom';
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+const startApp = () => {
+  render(<App />, document.getElementById('root'));
+};
+
+function SplashScreen(_props: { duration: any }) {
+  return (
+    <>
+      <h1>SplashScreen</h1> test test {_props.duration}
+    </>
+  );
+}
+
+render(<SplashScreen duration="4" />, document.getElementById('root'));
+
+setTimeout(startApp, 3000);
